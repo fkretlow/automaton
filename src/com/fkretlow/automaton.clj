c658937aca54cb172078bd9e4a004a9d15a1d50d
Florian Kretlow <florian.kretlow@innoq.com>
2025-10-31 15:28:48 +0100

    add reduce helper functions

--- src/com/fkretlow/automaton.clj
+++ src/com/fkretlow/automaton.clj
@@ -2,20 +2,41 @@
   (:require
    [com.fkretlow.automaton.coerce :refer [coerce-states]]))
 
-(defn make-machine
+(defn make-fsm
   [states & {:keys [data initial-state]}]
   {:state (or initial-state (first (keys states)))
    :data data,
    :_fsm/states (coerce-states states)})
 
-(defn process-event [machine event]
-  (let [{data :data, state-key :state, states :_fsm/states} machine
+(defn process-event [fsm event]
+  (let [{data :data, state-key :state, states :_fsm/states} fsm
         {:keys [compute-transition on-leave]} (get states state-key)
         [state-key' update-data] (compute-transition event data)
         is-state-change? (and state-key' (not= state-key state-key'))
         on-enter (get-in states [state-key' :on-enter])]
-    (cond-> machine
+    (cond-> fsm
       update-data (update :data update-data)
       (and is-state-change? on-leave) (update :data on-leave)
       is-state-change? (assoc :state state-key')
       (and is-state-change? on-enter) (update :data on-enter))))
+
+(defn- to-state-data-pair [{:keys [state data]}] [state data])
+
+(defn reduce-fsm
+  "Given an fsm and a seq of events, process the events one by one and return
+  the last state of the machine as a pair of the form `[state-key data]`."
+  [fsm events]
+  (to-state-data-pair (reduce process-event fsm events)))
+
+(defn iterate-fsm
+  "Given an fsm and a seq of events, process the events one by one and
+  return a lazy seq of all intermediary states as pairs of the form
+  `[state-key data]`. Useful for stopping event processing depending on
+  some reached state."
+  [fsm events]
+  (lazy-seq
+   (cons (to-state-data-pair fsm)
+         (when (seq events)
+           (iterate-fsm (process-event fsm (first events))
+                        (rest events))))))
+
--- test/com/fkretlow/automaton_test.clj
+++ test/com/fkretlow/automaton_test.clj
@@ -1,15 +1,20 @@
 (ns com.fkretlow.automaton-test
   (:require
    [clojure.test :refer [deftest is run-tests testing]]
-   [com.fkretlow.automaton :refer [make-machine process-event]]))
-
-(defn- reduce-fsm [fsm events] (:data (reduce process-event fsm events)))
+   [com.fkretlow.automaton :refer [iterate-fsm make-fsm process-event
+                                   reduce-fsm]]))
 
 (def ^:private count-ab-states {:0 [\a :a],
                                 :a [\a :a, \b :0 inc, :0]})
 
+(deftest test-iterate-fsm
+  (let [fsm (make-fsm count-ab-states :data 0)
+        states (iterate-fsm fsm "aba")]
+    (is (= [[:0 0] [:a 0] [:0 1] [:a 1]]
+           states))))
+
 (deftest test-process-event
-  (let [fsm (make-machine count-ab-states :data 0)
+  (let [fsm (make-fsm count-ab-states :data 0)
         fsm' (process-event fsm \a)
         fsm'' (process-event fsm' \b)]
     (is (= :a (:state fsm')))
@@ -18,15 +23,15 @@
     (is (= 1 (:data fsm'')))))
 
 (deftest test-states-as-functions
-  (let [fsm (make-machine {:0 (fn [c & _] (when (= \a c) :a))
-                           :a (fn [c & _] (if (= \b c) [:0 inc] :0))}
-                          :data 0)]
-    (is (= 3 (reduce-fsm fsm "ababab")))
-    (is (= 1 (reduce-fsm fsm "xxxaxabxxx"))))
-  (let [fsm (make-machine {:0 [\a :a]
-                           :a (fn [c & _] (case c \a :a, \b [:0 [inc inc]], :0))},
-                          :data 0)]
-    (is (= 6 (reduce-fsm fsm "ababab")))))
+  (let [fsm (make-fsm {:0 (fn [c & _] (when (= \a c) :a))
+                       :a (fn [c & _] (if (= \b c) [:0 inc] :0))}
+                      :data 0)]
+    (is (= 3 (second (reduce-fsm fsm "ababab"))))
+    (is (= 1 (second (reduce-fsm fsm "xxxaxabxxx")))))
+  (let [fsm (make-fsm {:0 [\a :a]
+                       :a (fn [c & _] (case c \a :a, \b [:0 [inc inc]], :0))},
+                      :data 0)]
+    (is (= 6 (second (reduce-fsm fsm "ababab"))))))
 
 (deftest test-pattern-example
   (testing "returns true if the pattern /(abc)+d/ exists in the input string"
@@ -36,16 +41,16 @@
            :b [\a :a, \c :c, :0],
            :c [\a :a, \d :d (constantly true), :0],
            :d nil}
-          fsm (make-machine states false)]
-      (is (reduce-fsm fsm "abcd"))
-      (is (reduce-fsm fsm "ababcd"))
-      (is (not (reduce-fsm fsm "abcxd")))
-      (is (reduce-fsm fsm "abcabcd"))
-      (is (reduce-fsm fsm "xxxabcabcdxxx"))
-      (is (not (reduce-fsm fsm "abd"))))))
+          fsm (make-fsm states false)]
+      (is (second (reduce-fsm fsm "abcd")))
+      (is (second (reduce-fsm fsm "ababcd")))
+      (is (not (second (reduce-fsm fsm "abcxd"))))
+      (is (second (reduce-fsm fsm "abcabcd")))
+      (is (second (reduce-fsm fsm "xxxabcabcdxxx")))
+      (is (not (second (reduce-fsm fsm "abd")))))))
 
 (deftest test-credentials-example
-  (let [fsm (make-machine
+  (let [fsm (make-fsm
              {:claim-pending
               (fn [claim _]
                 [:challenge-pending
@@ -57,14 +62,35 @@
     (let [{:keys [state data]} (reduce process-event fsm ["username" "pw"])]
       (is (= :success state))
       (is (= {:claim "username"} data)))
-    (is (not (:success (reduce-fsm fsm ["username" "bad password"]))))))
+    (is (not (:success (second (reduce-fsm fsm ["username" "bad password"])))))))
 
 (deftest test-binary-number-has-even-number-of-0s
-  (let [fsm (make-machine
+  (let [fsm (make-fsm
              {:even [\0 :uneven not],
               :uneven [\0 :even not]},
              :data true)]
-    (is (reduce-fsm fsm "0101010"))
-    (is (not (reduce-fsm fsm "010101")))))
+    (is (second (reduce-fsm fsm "0101010")))
+    (is (not (second (reduce-fsm fsm "010101"))))))
+
+(defn- isalnum? [c] (or (<= (int \a) (int c) (int \z))
+                        (<= (int \A) (int c) (int \Z))
+                        (<= (int \0) (int c) (int \9))))
+
+(deftest test-vim-word-object
+  (testing "can determine the end of the current/next word object"
+    (let [inc-index (fn [data] (update data :index inc))
+          inc-word-count (fn [data] (update data :word-count inc))
+          states {:start (fn [c _] (if (isalnum? c)
+                                     [:word [inc-index inc-word-count]]
+                                     [:not-word inc-index]))
+                  :word (fn [c _] (if (isalnum? c) [:word inc-index] [:not-word inc-index]))
+                  :not-word (fn [c _] (if (isalnum? c)
+                                        [:word [inc-index inc-word-count]]
+                                        [:not-word inc-index]))}
+          fsm (make-fsm states :data {:index -1, :word-count 0})
+          input " ab "]
+      (->> (iterate-fsm fsm input)
+           (drop-while (fn [[state data]] (or (= state :word) (zero? (:word-count data)))))
+           (prn)))))
 
 (run-tests)
